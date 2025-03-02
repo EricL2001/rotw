fa12b465a888bfa736596ce5dad921fca6cd740b
Eric Levasseur <eric.levass@gmail.com>
2025-03-02 11:14:25 -0500

    moved interface and data to seperate file

--- app/shows/page.tsx
+++ app/shows/page.tsx
@@ -1,60 +1,8 @@
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
-
+import { works } from "@/data"
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 
-export interface Artwork {
-  artist: string
-  venue: string
-  date: string
-  art: string
-  imageAlt: string
-}
-
-export const works: Artwork[] = [
-  {
-    artist: "Phish",
-    venue: "Madison Square Garden",
-    date: "Wed, Mar 25",
-    art: "/card-placeholder.jpg",
-    imageAlt: "Artwork by Ornella Binni",
-  },
-  {
-    artist: "AC/DC",
-    venue: "Wembley Stadium",
-    date: "Wed, Mar 25",
-    art: "/card-placeholder.jpg",
-    imageAlt: "Artwork by Ornella Binni"
-  },
-  {
-    artist: "Billy Strings",
-    venue: "Red Rocks Amphitheater",
-    date: "Wed, Mar 25",
-    art: "/card-placeholder.jpg",
-    imageAlt: "Artwork by Ornella Binni"
-  },
-  {
-    artist: "Dizgo",
-    venue: "The Mousetrap",
-    date: "Wed, Mar 25",
-    art: "/card-placeholder.jpg",
-    imageAlt: "Artwork by Ornella Binni",
-  },
-  {
-    artist: "Kitchen Dwellers",
-    venue: "The Fox Theater",
-    date: "Wed, Mar 25",
-    art: "/card-placeholder.jpg",
-    imageAlt: "Artwork by Ornella Binni"
-  },
-  {
-    artist: "Polyrhythmics",
-    venue: "The Bluebird",
-    date: "Wed, Mar 25",
-    art: "/card-placeholder.jpg",
-    imageAlt: "Artwork by Ornella Binni"
-  },
-]
 
 export default function ShowsPage() {
   return (
