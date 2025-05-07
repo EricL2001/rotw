5bdaf9b75a579b2c27549ada969677b30eab3651
Eric Levasseur <eric.levass@gmail.com>
2025-05-07 17:46:45 -0400

    more Sanity env variables update

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,9 +1,12 @@
 export const apiVersion: string = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-14';
 
 //export const dataset: string = process.env.NEXT_PUBLIC_SANITY_DATASET || "";
-export const dataset = 'ticket-prod'; // Use a hardcoded value for testing
 
-export const projectId: string = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
+//export const projectId: string = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
+
+
+export const dataset: string = process.env.SANITY_STUDIO_DATASET || 'ticket-prod';
+export const projectId: string = process.env.SANITY_STUDIO_PROJECT_ID || '47h2dt9x';
 
 //console.log("Dataset from env:", dataset); 
 //console.log("ProjectID from env:", projectId); 
