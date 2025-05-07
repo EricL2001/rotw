ba4a6b0a44be3d18fa36df61ebf74476b6553880
Eric Levasseur <eric.levass@gmail.com>
2025-05-07 17:25:07 -0400

    added hard coded dataset to test

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,6 +1,7 @@
 export const apiVersion: string = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-14';
 
-export const dataset: string = process.env.NEXT_PUBLIC_SANITY_DATASET || "";
+//export const dataset: string = process.env.NEXT_PUBLIC_SANITY_DATASET || "";
+export const dataset = 'ticket-prod'; // Use a hardcoded value for testing
 
 export const projectId: string = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
 
