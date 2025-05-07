9b5d4590e7fc87bd9d975ea991a40596ccc55c1c
Eric Levasseur <eric.levass@gmail.com>
2025-05-07 17:13:45 -0400

    added more debug help

--- sanity/env.ts
+++ sanity/env.ts
@@ -4,13 +4,13 @@ export const dataset: string = process.env.NEXT_PUBLIC_SANITY_DATASET || "";
 
 export const projectId: string = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
 
-console.log("Dataset from env:", dataset); 
-console.log("ProjectID from env:", projectId); 
+//console.log("Dataset from env:", dataset); 
+//console.log("ProjectID from env:", projectId); 
 
 if (!dataset) {
-    throw new Error('Missing the fucking environment variable: NEXT_PUBLIC_SANITY_DATASET');
+    throw new Error(`Missing the environment variable: NEXT_PUBLIC_SANITY_DATASET. Dataset from env: ${dataset}`);
 }
 
 if (!projectId) {
-    throw new Error('Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID');
+    throw new Error(`Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID.  ProjectID from env: ${projectId}`);
 }
