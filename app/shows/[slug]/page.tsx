b43595483419e2e43dc6bee42e28b5070a629b90
Eric Levasseur <eric.levass@gmail.com>
2025-11-03 17:17:41 -0500

    updated metadata on slug page

--- app/shows/[slug]/page.tsx
+++ app/shows/[slug]/page.tsx
@@ -69,12 +69,12 @@ export async function generateMetadata({
   return {
     title: show.showType === 'Free'
       ? `${show.title} at ${show.venue}`
-      : `Tickets On Sale For ${show.title} • Records On The Wall`,
+      : `Tickets On Sale For ${show.title} at ${show.venue}`,
     description: `${show.showType === 'Free' ? 'Free show!' : `Grab tickets to see ${show.title} at ${show.venue}.`}`,
     openGraph: {
       title: show.showType === 'Free'
         ? `${show.title} at ${show.venue}`
-        : `Tickets On Sale For ${show.title} • Records On The Wall`,
+        : `Tickets On Sale For ${show.title} at ${show.venue}`,
       description: `${show.showType === 'Free' ? 'Free show!' : `Grab tickets to see ${show.title} at ${show.venue}.`}`,
       url: `https://recordsonthewall.co/shows/${resolvedParams.slug}`,
       siteName: 'Records On The Wall',
